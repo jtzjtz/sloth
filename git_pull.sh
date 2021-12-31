3742729e4af1b09cf351ac3254b1c5ee1ab8edec
github.com/jtzjtz/ys_pack <github.com/jtzjtz/ys_pack@163.com>
2021-12-31 19:11:08 +0800

    自动化构建

--- git_pull.sh
+++ git_pull.sh
@@ -3,7 +3,7 @@ cd $1
 rm -rf "$3"
 
 echo "开始拉取代码 cd $1<br>"
-git clone https://$4:$5%@$2.git $3
+git clone https://$4:$5@$2.git $3
 echo "拉取代码完成<br>"
 git config --global user.email "$4@163.com"
 git config --global user.name "$4"
